f35e5a7b0270cec55862f129d71c22527bd47b06
sahan111 <115946371+sahan111@users.noreply.github.com>
2022-10-18 23:00:09 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InlineAssignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
